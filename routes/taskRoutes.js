90edd7a1c60f573f17269e002c35a5f99bfd6a87
milan julinac <milanjulinac996@gmail.com>
2023-12-30 17:25:17 +0100

    added basic validation

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -1,13 +1,61 @@
 const express = require("express");
+const { body } = require("express-validator");
 const taskController = require("../contollers/taskController");
+const { validate } = require("../middleware/validationMiddleware");
 
 const router = express.Router();
 
 router.get("/", taskController.getAllTasks);
-router.post("/", taskController.createTask);
-router.patch("/:id", taskController.updateTask);
-router.delete("/:id", taskController.deleteTask);
-router.patch("/:id/complete", taskController.completeTask);
-router.patch("/:id/cancel", taskController.cancelTask);
+
+router.post(
+    "/",
+    [
+        body("title").notEmpty().withMessage("Title is required"),
+        body("description").optional(),
+        body("dueDate")
+            .optional()
+            .isISO8601()
+            .toDate()
+            .withMessage("Invalid date format"),
+    ],
+    validate,
+    taskController.createTask
+);
+
+router.patch(
+    "/:id",
+    [
+        body("title").optional(),
+        body("description").optional(),
+        body("dueDate")
+            .optional()
+            .isISO8601()
+            .toDate()
+            .withMessage("Invalid date format"),
+    ],
+    validate,
+    taskController.updateTask
+);
+
+router.delete(
+    "/:id",
+    [param("id").notEmpty().withMessage("Task ID is required")],
+    validate,
+    taskController.deleteTask
+);
+
+router.patch(
+    "/:id/complete",
+    [param("id").notEmpty().withMessage("Task ID is required")],
+    validate,
+    taskController.completeTask
+);
+
+router.patch(
+    "/:id/cancel",
+    [param("id").notEmpty().withMessage("Task ID is required")],
+    validate,
+    taskController.cancelTask
+);
 
 module.exports = router;
