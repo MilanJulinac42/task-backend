d2026fbf3bd05225fa36720ed2d254aa4e0872f3
milan julinac <milanjulinac996@gmail.com>
2024-01-02 08:49:45 +0100

    added cors

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const taskRoutes = require("./routes/taskRoutes");
 const loggingMiddleware = require("./middleware/loggingMiddleware");
+const cors = require("cors");
 
 const app = express();
 const PORT = process.env.PORT || 3002;
@@ -10,6 +11,7 @@ mongoose.connect("mongodb://localhost:27017/tasktracker", {});
 
 app.use(express.json());
 app.use(loggingMiddleware);
+app.use(cors());
 
 app.use("/tasks", taskRoutes);
 
